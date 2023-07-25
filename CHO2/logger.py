3ceeac66fabb696b2fa5aa7715bcfe6c1252228c
Raell Dottin <raell.dottin@gmail.com>
2023-07-25 10:25:51 -0400

    add log levels

--- CHO2/logger.py
+++ CHO2/logger.py
@@ -1,3 +1,28 @@
 def log_message(msg):
     with open("filename.log", "a") as log_file:
         log_file.write("{0}\n".format(msg))
+
+
+def critical(msg):
+    with open("filename.log", "a") as log_file:
+        log_file.write("[CRITICAL] {0}\n".format(msg))
+
+
+def error(msg):
+    with open("filename.log", "a") as log_file:
+        log_file.write("[ERROR] {0}\n".format(msg))
+
+
+def warn(msg):
+    with open("filename.log", "a") as log_file:
+        log_file.write("[WARN] {0}\n".format(msg))
+
+
+def info(msg):
+    with open("filename.log", "a") as log_file:
+        log_file.write("[INFO] {0}\n".format(msg))
+
+
+def debug(msg):
+    with open("filename.log", "a") as log_file:
+        log_file.write("[DEBUG] {0}\n".format(msg))
