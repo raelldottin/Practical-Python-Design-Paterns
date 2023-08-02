94c23861492111fffd9a5218e2756015017ba6d7
Raell Dottin <raell.dottin@gmail.com>
2023-08-02 16:30:49 -0400

    add unit creation and output current state

--- CH03/rts_mutli_unit.py
+++ CH03/rts_mutli_unit.py
@@ -76,3 +76,7 @@ class Barracks(object):
 
 if __name__ == "__main__":
     barracks = Barracks()
+    knight1 = barracks.build_unit("knight", 1)
+    archer1 = barracks.build_unit("archer", 2)
+    print("[knight1] {}".format(knight1))
+    print("[archer1] {}".format(archer1))
