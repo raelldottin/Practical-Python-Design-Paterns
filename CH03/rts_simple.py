8e25ebe38268441e7675c7c40b0c720d170b37fd
Raell Dottin <raell.dottin@gmail.com>
2023-08-02 16:05:08 -0400

    add archer class

--- CH03/rts_simple.py
+++ CH03/rts_simple.py
@@ -1,5 +1,6 @@
 class Knight(object):
     def __init__(self, life, speed, attack_power, attack_range, weapon):
+        self.unit_type = "Knight"
         self.life = life
         self.speed = speed
         self.attack_power = attack_power
@@ -8,12 +9,45 @@ class Knight(object):
 
     def __str__(self):
         return (
-            "Life: {0}\n"
-            "Speed: {1}\n"
-            "Attack Power: {2}\n"
-            "Attack Range: {3}\n"
-            "Weapon: {4}".format(
-                self.life, self.speed, self.attack_power, self.attack_range, self.weapon
+            "Type: {0}\n"
+            "Life: {1}\n"
+            "Speed: {2}\n"
+            "Attack Power: {3}\n"
+            "Attack Range: {4}\n"
+            "Weapon: {5}".format(
+                self.unit_type,
+                self.life,
+                self.speed,
+                self.attack_power,
+                self.attack_range,
+                self.weapon,
+            )
+        )
+
+
+class Archer(object):
+    def __init__(self, life, speed, attack_power, attack_range, weapon):
+        self.unit_type = "Archer"
+        self.life = life
+        self.speed = speed
+        self.attack_power = attack_power
+        self.attack_range = attack_range
+        self.weapon = weapon
+
+    def __str__(self):
+        return (
+            "Type: {0}\n"
+            "Life: {1}\n"
+            "Speed: {2}\n"
+            "Attack Power: {3}\n"
+            "Attack Range: {4}\n"
+            "Weapon: {5}".format(
+                self.unit_type,
+                self.life,
+                self.speed,
+                self.attack_power,
+                self.attack_range,
+                self.weapon,
             )
         )
 
@@ -22,8 +56,13 @@ class Barracks(object):
     def generate_knight(self):
         return Knight(400, 5, 3, 1, "short sword")
 
+    def generate_archer(self):
+        return Archer(200, 7, 1, 5, "short bow")
+
 
 if __name__ == "__main__":
     barracks = Barracks()
     knight1 = barracks.generate_knight()
+    archer1 = barracks.generate_archer()
     print("[knight1] {}".format(knight1))
+    print("[archer1] {}".format(archer1))
