61d5e0f27b3ef373ebe1ba9f5c1db9aec9377e89
Raell Dottin <raell.dottin@gmail.com>
2023-08-17 17:38:16 -0400

    add a red rectangle to the window

--- CH04/graphic_base.py
+++ CH04/graphic_base.py
@@ -1,8 +1,10 @@
 import pygame
-from time import sleep
+import time
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
+pygame.draw.rect(screen, (255, 0, 34), pygame.Rect(42, 15, 40, 32))
+pygame.display.flip()
 
-sleep(10)
+time.sleep(10)
