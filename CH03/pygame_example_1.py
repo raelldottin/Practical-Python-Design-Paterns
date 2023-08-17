1551e95517aed42d29cd67c44c7f84173ab77288
Raell Dottin <raell.dottin@gmail.com>
2023-08-17 17:31:51 -0400

    add pygame example 1

---
+++ CH03/pygame_example_1.py
@@ -0,0 +1,26 @@
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put youur work on screen
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
