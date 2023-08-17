983a221de96878df12e199ba57c80b2fb8f28216
Raell Dottin <raell.dottin@gmail.com>
2023-08-16 21:19:14 -0400

    add more logger function calls

--- CH02/logger_singleton.py
+++ CH02/logger_singleton.py
@@ -1,9 +1,8 @@
-
 class Logger(object):
-    class __Logger():
+    class __Logger:
         def __init__(self, file_name):
             self.file_name = file_name
-        
+
         def _write_log(self, level, msg):
             with open(self.file_name, "a") as log_file:
                 log_file.write("[{0}] {1}\n".format(level, msg))
@@ -13,33 +12,42 @@ class Logger(object):
 
         def error(self, msg):
             self._write_log("ERROR", msg)
-        
+
         def warn(self, msg):
             self._write_log("WARN", msg)
-            
+
         def info(self, msg):
             self._write_log("INFO", msg)
-        
+
         def debug(self, msg):
             self._write_log("DEBUG", msg)
-        
+
     instance = None
-    
+
     def __new__(cls, file_name):
         if not Logger.instance:
             Logger.instance = Logger.__Logger(file_name)
-            
+
         return Logger.instance
 
     def __getattr__(self, name):
         return getattr(self.instance, name)
-        
+
     def __setattr__(self, name):
         return setattr(self.instance, name)
-        
-        
+
+
 logger_object = Logger("class_logger_1.log")
-logger_object.info("This is an info message")
+logger_object.critical("This is an info message")
 
 logger_object = Logger("class_logger_2.log")
 logger_object.error("This is an error message")
+
+logger_object = Logger("class_logger_1.log")
+logger_object.warn("This is an info message")
+
+logger_object = Logger("class_logger_2.log")
+logger_object.info("This is an error message")
+
+logger_object = Logger("class_logger_1.log")
+logger_object.debug("This is an info message")
