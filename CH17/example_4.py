ca1d5dc59197debf2d198992ed9a4ab2a3f945cf
Raell Dottin <raell.dottin@gmail.com>
2023-09-06 13:43:53 -0400

    add example 4

---
+++ CH17/example_4.py
@@ -0,0 +1,30 @@
+def sync_stock_items(strategy_func):
+    strategy_func()
+
+
+def send_transaction(transaction, strategy_func):
+    strategy_func(transaction)
+
+
+def stock_sync_strategy_system1():
+    print("running stock sync between local and remote system1")
+    print("retrieving remote stock items from system1")
+    print("updating local items")
+    print("sending updates to third party system1")
+
+
+def stock_sync_strategy_system2():
+    print("running stock sync between local and remote system2")
+    print("retrieving remote stock items from system2")
+    print("updating local items")
+    print("sending updates to third party system2")
+
+
+def stock_sync_strategy_system3():
+    print("running stock sync between local and remote system3")
+    print("retrieving remote stock items from system3")
+    print("updating local items")
+    print("sending updates to third party system3")
+
+
+def send_transaction_strategy_system
