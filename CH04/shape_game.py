129a5834349a609dade2404dd7061ff7de2f9acb
Raell Dottin <raell.dottin@gmail.com>
2023-09-07 20:08:46 -0400

    change shape game source

--- CH04/shape_game.py
+++ CH04/shape_game.py
@@ -1,10 +1,41 @@
 import pygame
 
-window_dimensions = 800, 600
-screen = pygame.display.set_mode(window_dimensions)
 
-x = 100
-y = 100
+class Shape(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self):
+        raise NotImplementedError()
+
+    def move(self, direction):
+        if direction == "up":
+            self.y -= 4
+        elif direction == "down":
+            self.y += 4
+        elif direction == "left":
+            self.x -= 4
+        elif direction == "right":
+            self.x += 4
+
+
+class Square(Shape):
+    def draw(self):
+        pygame.draw.rect(screen, (255, 255, 0), pygame.Rect(self.x, self.y, 20, 20))
+
+
+class Circle(Shape):
+    def draw(self):
+        pygame.draw.circle(screen, (0, 255, 255), (self.x, self.y), 10)
+
+
+if __name__ == "__main__":
+    window_dimensions = 800, 600
+    screen = pygame.display.set_mode(window_dimensions)
+
+square = Square(100, 100)
+obj = square
 
 player_quits = False
 
@@ -15,15 +46,18 @@ while not player_quits:
 
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_UP]:
-            y -= 4
+            obj.move("up")
+
         if pressed[pygame.K_DOWN]:
-            y += 4
+            obj.move("down")
+
         if pressed[pygame.K_LEFT]:
-            x -= 4
+            obj.move("left")
+
         if pressed[pygame.K_RIGHT]:
-            x += 4
+            obj.move("right")
 
         screen.fill((0, 0, 0))
-        pygame.draw.rect(screen, (255, 255, 0), pygame.Rect(x, y, 20, 20))
+        obj.draw()
 
     pygame.display.flip()
