f9071f87dbdd6745f810ebcb2d713ede8e1dcfa0
Raell Dottin <raell.dottin@gmail.com>
2023-10-11 20:30:56 -0400

    Refactor Logger class to implement the Singleton pattern for a file-based message logger.

--- Singleton-Pattern/exercise_1.py
+++ Singleton-Pattern/exercise_1.py
@@ -4,42 +4,45 @@ Chapter 2, Exercise 1
 """
 
 
-class Logger:
-    """A file-based message logger with the following properties
+class Logger(object):
+    class __Logger:
+        def __init__(self, file_name):
+            """Return a Logger object whose file_name is *file_name*"""
+            self.file_name = file_name
 
-    Attributers:
-        file_name: a string representing the full path of the log file to which this logger will write its messages
-    """
+        def __str__(self):
+            return "{0!r} {1}".format(self, self.file_name)
+
+        def _write_log(self, level, msg):
+            """Writes a message to the file_name for a specific Logger instance"""
+            with open(self.file_name, "a") as log_file:
+                log_file.write("[{0}] {1}\n".format(level, msg))
+
+        def critical(self, msg, level="CRITICAL"):
+            self._write_log(level, msg)
+
+        def error(self, msg, level="ERROR"):
+            self._write_log(level, msg)
+
+        def warn(self, msg, level="WARN"):
+            self._write_log(level, msg)
+
+        def info(self, msg, level="INFO"):
+            self._write_log(level, msg)
+
+        def debug(self, msg, level="DEBUG"):
+            self._write_log(level, msg)
 
     _instance = None
 
     def __new__(cls, file_name):
-        """Return the class object if the class was already created; otherwise, create a new class"""
-        if cls._instance == None:
-            cls._instance = super(Logger, cls).__init__(file_name)
+        if not Logger._instance:
+            Logger._instance = Logger.__Logger(file_name)
 
-        return cls._instance
+        return Logger._instance
 
-    def __init__(self, file_name):
-        """Return a Logger object whose file_name is *file_name*"""
-        self.file_name = file_name
+    def __getattr__(self, name):
+        return getattr(self._instance, name)
 
-    def _write_log(self, level, msg):
-        """Writes a message to the file_name for a specific Logger instance"""
-        with open(self.file_name, "a") as log_file:
-            log_file.write("[{0}] {1}\n".format(level, msg))
-
-    def critical(self, msg, level="CRITICAL"):
-        self._write_log(level, msg)
-
-    def error(self, msg, level="ERROR"):
-        self._write_log(level, msg)
-
-    def warn(self, msg, level="WARN"):
-        self._write_log(level, msg)
-
-    def info(self, msg, level="INFO"):
-        self._write_log(level, msg)
-
-    def debug(self, msg, level="DEBUG"):
-        self._write_log(level, msg)
+    def __setattr__(self, name):
+        return setattr(self._instance, name)
