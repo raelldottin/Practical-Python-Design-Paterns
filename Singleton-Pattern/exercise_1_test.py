c4f09736df2b10948623b1d74ab82a46eed28722
Raell Dottin <raell.dottin@gmail.com>
2023-10-11 20:31:44 -0400

    Add test cases for the Logger class in exercise_1_test.py.

--- Singleton-Pattern/exercise_1_test.py
+++ Singleton-Pattern/exercise_1_test.py
@@ -1,16 +1,23 @@
+import os.path
 from exercise_1 import Logger
 
 
 def read_log(file_name):
+    if not os.path.exists(file_name):
+        print("{0} doesn't exists", file_name)
+        return
+
     with open(file_name, "r") as log_file:
-        log_file.read()
+        print(log_file.read())
 
 
 def main():
     obj1 = Logger("class_logger_object_1.log")
+    print(dir(obj1))
     obj1.info("This is na info message for obj1")
     read_log("class_logger_object_1.log")
     obj2 = Logger("class_logger_object_2.log")
+    print(dir(obj2))
     obj2.info("This is na info message for obj2")
     read_log("class_logger_object_2.log")
 
