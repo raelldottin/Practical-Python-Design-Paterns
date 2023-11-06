4db15b935efa2fc98649a69c881c6d71a6b7d7d0
Raell Dottin <raell.dottin@gmail.com>
2023-11-05 22:12:44 -0500

    Refactor logging output and correct typos in exercise_1_test.py

--- Singleton-Pattern/exercise_1_test.py
+++ Singleton-Pattern/exercise_1_test.py
@@ -4,26 +4,23 @@ from exercise_1 import Logger
 
 def read_log(file_name):
     if not os.path.exists(file_name):
-        print("{0} doesn't exists", file_name)
+        print("{0} doesn't exists".format(file_name))
         return
 
+    print("{0} contains:".format(file_name))
     with open(file_name, "r") as log_file:
         print(log_file.read())
 
 
 def main():
     obj1 = Logger("class_logger_object_1.log")
-    print(dir(obj1))
-    obj1.info("This is na info message for obj1")
+    print("{0} contain the following objects: {1}".format(obj1, dir(obj1)))
+    obj1.info("This is an info message for obj1")
     read_log("class_logger_object_1.log")
     obj2 = Logger("class_logger_object_2.log")
-    print(dir(obj2))
-    obj2.info("This is na info message for obj2")
+    print("{0} contain the following objects: {1}".format(obj2, dir(obj2)))
+    obj2.info("This is an info message for obj2")
     read_log("class_logger_object_2.log")
-
-    print("print obj1: ", obj1)
-    print("print obj2: ", obj2)
-
 
 if __name__ == "__main__":
     main()
